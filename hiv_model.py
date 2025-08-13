0fa74aa82c58bd96072cd2cf22b5280a7aedc8fe
Robyn Stuart <robyna.s@gmail.com>
2025-08-13 13:06:04 +1000

    more agents

--- hiv_model.py
+++ hiv_model.py
@@ -3,43 +3,15 @@ Create HIV model and interventions
 """
 
 # %% Imports and settings
-import numpy as np
-import starsim as ss
 import sciris as sc
 import pandas as pd
 import stisim as sti
 from interventions import make_hiv_intvs
 
 
-def make_hiv():
-    """ Make HIV arguments for sim"""
-    hiv = sti.HIV(
-        beta_m2f=0.035,
-        eff_condom=0.95,
-        init_prev_data=pd.read_csv('data/init_prev_hiv.csv'),
-        rel_init_prev=1.,
-    )
-    return hiv
+def make_sim(verbose=1/12, analyzers=None):
 
-
-def make_sim(seed=1, n_agents=None, start=1990, stop=2030, debug=False, verbose=1/12, analyzers=None):
-
-    total_pop = {1990: 7.786e6}[start]
-    if n_agents is None: n_agents = [int(10e3), int(5e2)][debug]
-
-    ####################################################################################################################
-    # Demographic modules
-    ####################################################################################################################
-    fertility_data = pd.read_csv(f'data/asfr.csv')
-    pregnancy = ss.Pregnancy(unit='month', fertility_rate=fertility_data)
-    death_data = pd.read_csv(f'data/deaths.csv')
-    death = ss.Deaths(death_rate=death_data, rate_units=1)
-
-    ####################################################################################################################
-    # People and networks
-    ####################################################################################################################
-    ppl = ss.People(n_agents, age_data=pd.read_csv(f'data/age_dist_{start}.csv', index_col='age')['value'])
-    sexual = sti.StructuredSexual(
+    nw_pars = dict(
         prop_f0=0.79,
         prop_m0=0.83,
         f1_conc=0.16,
@@ -47,26 +19,23 @@ def make_sim(seed=1, n_agents=None, start=1990, stop=2030, debug=False, verbose=
         p_pair_form=0.58,
         condom_data=pd.read_csv(f'data/condom_use.csv'),
     )
-    # msm = sti.AgeMatchedMSM(
-    #     participation=0.1,
-    # )
-    maternal = ss.MaternalNet(unit='month')
 
-    ####################################################################################################################
-    # Diseases and interventions
-    ####################################################################################################################
-    hiv = make_hiv()
-    diseases = [hiv]
+    hiv = sti.HIV(
+        beta_m2f=0.1,
+        eff_condom=0.95,
+        init_prev_data=pd.read_csv('data/init_prev_hiv.csv'),
+        rel_init_prev=5.,
+    )
+
     intvs = make_hiv_intvs()
 
     sim = sti.Sim(
-        total_pop=total_pop,
-        start=start,
-        stop=stop,
-        people=ppl,
-        diseases=diseases,
-        networks=[sexual, maternal],
-        demographics=[pregnancy, death],
+        n_agents=10e3,
+        start=1990,
+        stop=2030,
+        demographics='zambia',
+        diseases=hiv,
+        nw_pars=nw_pars,
         interventions=intvs,
         analyzers=analyzers,
         verbose=verbose,
@@ -83,8 +52,15 @@ if __name__ == '__main__':
     do_save = True
     do_run = True
 
-    sim = make_sim(start=1990, stop=2041)
+    sim = make_sim()
     sim.run()
-    df = sim.to_df(resample='year', use_years=True, sep='.')  # Use dots to separate columns
+
+    # Export to dataframe and plot
+    df = sim.to_df(resample='year', use_years=True, sep='_')  # Use dots to separate columns
+    df.index = df['timevec']
+
+    from plot_sims import plot_hiv_sims
+    plot_hiv_sims(df, title='hiv_plots')
+
     if do_save: sc.saveobj(f'results/zambia_sim.df', df)
 
