adac35a2f24847f0ad65cf27703c74d82f101502
Robyn Stuart <robyna.s@gmail.com>
2025-08-15 14:03:27 +1000

    run versoin

--- hiv_model.py
+++ hiv_model.py
@@ -59,7 +59,8 @@ def make_sim(seed=1, n_agents=None, start=1990, stop=2030, debug=False, verbose=
     diseases = [hiv]
     intvs = make_hiv_intvs()
 
-    sim = sti.Sim(
+    sim = ss.Sim(
+        dt=1/12,
         total_pop=total_pop,
         start=start,
         stop=stop,
@@ -85,6 +86,10 @@ if __name__ == '__main__':
 
     sim = make_sim(start=1990, stop=2041)
     sim.run()
-    df = sim.to_df(resample='year', use_years=True, sep='.')  # Use dots to separate columns
+    df = sim.to_df(resample='year', use_years=True, sep='_')  # Use dots to separate columns
+    df.index = df['timevec']
     if do_save: sc.saveobj(f'results/zambia_sim.df', df)
+
+    from plot_sims import plot_hiv_sims
+    plot_hiv_sims(df, start_year=1990)
 
