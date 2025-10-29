17499caae968c9f48f596f3fb94a58eeba0d5a4a
Robyn Stuart <robyna.s@gmail.com>
2025-10-29 16:39:47 +1100

    write par setter

--- run_hiv_calibration.py
+++ run_hiv_calibration.py
@@ -15,7 +15,7 @@ os.environ.update(
 import sciris as sc
 import stisim as sti
 import pandas as pd
-from hiv_model import make_sim
+from hiv_model import make_sim, make_sim_pars
 
 
 # Run settings
@@ -26,33 +26,6 @@ n_workers = [50, 1][debug]    # How many cores to use
 storage = None
 do_shrink = True  # Whether to shrink the calibration results
 make_stats = True  # Whether to make stats
-
-
-def build_sim(sim, calib_pars):
-    if not sim.initialized: sim.init()
-    hiv = sim.diseases.hiv
-    nw = sim.networks.structuredsexual
-
-    # Apply the calibration parameters
-    for k, pars in calib_pars.items():  # Loop over the calibration parameters
-        if k == 'rand_seed':
-            sim.pars.rand_seed = v
-            continue
-
-        v = pars['value']
-        if 'hiv_' in k:  # HIV parameters
-            k = k.replace('hiv_', '')  # Strip off indentifying part of parameter name
-            hiv.pars[k] = v
-        elif 'nw_' in k:  # Network parameters
-            k = k.replace('nw_', '')  # As above
-            if 'pair_form' in k:
-                nw.pars[k].set(v)
-            else:
-                nw.pars[k] = v
-        else:
-            raise NotImplementedError(f'Parameter {k} not recognized')
-
-    return sim
 
 
 def run_calibration(n_trials=None, n_workers=None, do_save=True):
@@ -75,7 +48,7 @@ def run_calibration(n_trials=None, n_workers=None, do_save=True):
     # Make the calibration
     calib = sti.Calibration(
         calib_pars=calib_pars,
-        build_fn=build_sim,
+        build_fn=make_sim_pars,
         sim=sim,
         extra_results=extra_results,
         data=data,
